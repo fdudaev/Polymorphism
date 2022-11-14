be17bfd5d870b2fe01f7f458212302b8795a230d
fdudaev <61507529+fdudaev@users.noreply.github.com>
2022-11-14 17:36:03 -0500

    comminting OOP polymorphism for color

--- src/Car.java
+++ src/Car.java
@@ -5,6 +5,8 @@ class Car {
     private String name;
     private int wheels;
 
+    private String color;
+
     public Car(int cylinders, String name) {
         this.cylinders = cylinders;
         this.name = name;
