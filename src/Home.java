9100d90325afcfe11d42b8f938a5a3a46b1d6d4d
fdudaev <61507529+fdudaev@users.noreply.github.com>
2022-11-14 18:04:20 -0500

    adding new class Home

---
+++ src/Home.java
@@ -0,0 +1,20 @@
+public class Home {
+
+    private String color;
+    private int numberOfDoors;
+    private int numberOfWindows;
+    private int numberOfRooms;
+    private int numberOfRestRooms;
+    private  int garages;
+    private int stories;
+
+    public Home(String color, int numberOfDoors, int numberOfWindows, int numberOfRooms, int numberOfRestRooms, int garages, int stories) {
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+        this.numberOfWindows = numberOfWindows;
+        this.numberOfRooms = numberOfRooms;
+        this.numberOfRestRooms = numberOfRestRooms;
+        this.garages = garages;
+        this.stories = stories;
+    }
+}
